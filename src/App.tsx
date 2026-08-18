2a4ea9a71fca617ed3a66afc021d094e95a8a376
Negar Missaghian <negar.misaghian70@gmail.com>
2026-08-18 20:29:17 +0430

    Initial MyDay setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Header.tsx' ;
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <p>Content  will be coming soon...</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function Header() {
+
+  return (
+    <header>
+      <h1>MyDay</h1>
+      <button>🌙</button>
+    </header>
+  )
+}
+
+export default Header
