5a81e603bec1260ef460e8cce25e80bdc5fb7c9f
Negar Missaghian <negar.misaghian70@gmail.com>
2026-08-19 13:15:24 +0430

    Build and style header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Header from './Header.tsx' ;
 
 function App() {
@@ -6,7 +5,6 @@ function App() {
   return (
     <div>
       <Header />
-      <p>Content  will be coming soon...</p>
     </div>
   )
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,17 @@
-import './App.css'
+import './Header.css'
+
 
 function Header() {
 
   return (
-    <header>
-      <h1>MyDay</h1>
-      <button>🌙</button>
-    </header>
+    <header className='header'>
+      <div>
+          <h1>MyDay</h1>
+          <h2>Good Evening Negar 🖐</h2>
+          <p>Plan your day, your way</p>
+      </div>
+      <p>🌙</p>
+      </header>
   )
 }
 
